68604db7d350cb876240221f3310efd3657b472c
Vadim Kaushan <admin@disasm.info>
2020-03-02 01:00:49 +0300

    Use the released ili9341 version, update examples

--- examples/display.rs
+++ examples/display.rs
@@ -9,9 +9,10 @@ use gd32vf103xx_hal::pac;
 use seedstudio_gd32v::lcd_pins;
 use seedstudio_gd32v::lcd::Lcd;
 use embedded_graphics::prelude::*;
-use embedded_graphics::fonts::Font12x16;
+use embedded_graphics::fonts::{Font12x16, Text};
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::primitives::Rectangle;
+use embedded_graphics::style::{PrimitiveStyle, TextStyleBuilder};
 use ili9341::Orientation;
 
 #[entry]
@@ -31,9 +32,16 @@ fn main() -> ! {
     lcd.set_orientation(Orientation::Landscape).unwrap();
 
     let p = Point::new(lcd.width() as i32 - 1, lcd.height() as i32 - 1);
-    lcd.draw(Rectangle::new(Point::new(0, 0), p).fill(Some(Rgb565::BLACK)));
-    let t = Font12x16::render_str(" Hello Rust! ").fill(Some(Rgb565::GREEN)).translate(Point::new(40, 35));
-    lcd.draw(t);
+    Rectangle::new(Point::new(0, 0), p)
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK))
+        .draw(&mut *lcd).unwrap();
+
+    let style = TextStyleBuilder::new(Font12x16)
+        .background_color(Rgb565::GREEN)
+        .build();
+    Text::new(" Hello Rust! ", Point::new(40, 35))
+        .into_styled(style)
+        .draw(&mut *lcd).unwrap();
 
     loop { }
 }
--- examples/ferris.rs
+++ examples/ferris.rs
@@ -9,8 +9,10 @@ use gd32vf103xx_hal::pac;
 use seedstudio_gd32v::lcd_pins;
 use seedstudio_gd32v::lcd::{Lcd, ili9341::Orientation};
 use embedded_graphics::prelude::*;
-use embedded_graphics::pixelcolor::{Rgb565};
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::image::{Image, ImageRaw};
 use embedded_graphics::primitives::Rectangle;
+use embedded_graphics::style::PrimitiveStyle;
 
 const FERRIS: &[u8] = include_bytes!("ferris.raw");
 
@@ -31,11 +33,13 @@ fn main() -> ! {
     lcd.set_orientation(Orientation::Landscape).unwrap();
 
     let p = Point::new(lcd.width() as i32 - 1, lcd.height() as i32 - 1);
-    lcd.draw(Rectangle::new(Point::new(0, 0), p).fill(Some(Rgb565::BLACK)));
+    Rectangle::new(Point::new(0, 0), p)
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK))
+        .draw(&mut *lcd).unwrap();
 
-    let image: Image<Rgb565> = Image::new(&FERRIS, 86, 64);
-    let image = image.translate(Point::new(320/2 - 43, 240/2 - 32));
-    lcd.draw(&image);
+    let image: ImageRaw<Rgb565> = ImageRaw::new(&FERRIS, 86, 64);
+    Image::new(&image, Point::new(320/2 - 43, 240/2 - 32))
+        .draw(&mut *lcd).unwrap();
 
     loop { }
 }
